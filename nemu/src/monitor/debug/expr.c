29bb06fa24a5c2f0c00b0d6ead51146a2a5fc1e2
tracer-ics2018 <tracer@njuics.org>
2018-09-21 14:14:55 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  14:14:55 up 11:52,  1 user,  load average: 0.08, 0.04, 0.06 10974292290dd3d4f390376d1544a77f37cbe6e3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -140,7 +140,11 @@ static int eval(int p,int q){
 				case '+': return val1+val2;
                                 case '-': return val1-val2;
 				case '*': return val1*val2;
-				case '/': return val1/val2;
+				case '/': if(val2==0){
+						  detect=0;
+						  return 0;}
+					  else{
+						  return val1/val2;}
 				default: assert(0);
 			}
 			}
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -6,11 +6,21 @@
 #include <string.h>
 
 // this should be enough
-static unsigned gen_num(){
-       return rand()%4294967296 +0;}
+static int gen_num(char *s){
+       int temp=rand()%4294967296 +0;
+       int k=0;
+       int count=0;
+       while(temp!=0){
+         k=temp%10;
+	 temp=(temp-k)/10;
+         *(s+count)=(char)k+'0';
+	 count++;
+       }
+       return count;}
+
 static int choose(){
        return rand()%3 +0;}
-static int gen_rand_op(){
+static char gen_rand_op(){
        int temp=rand()4 +1;
        switch(temp){
 	       case 1: return '+';
@@ -20,11 +30,16 @@ static int gen_rand_op(){
 	       default: return ' ';}
 }	
 static char buf[65536];
+int label=0;
 static inline void gen_rand_expr() {
   buf[0] = '\0';
-  int label;
   switch(choose()){
-	  case 0:buf[
+	  case 0:char a[11];int len=gen_num(a);for(int i=0;i<len;i++){
+			 buf[label++]=a[i];}break;
+          case 1:buf[label++]='(';gen_rand_expr();buf[label++]=')';break;
+          default:gen_rand_expr();buf[label++]=gen_rand_op();gen_rand_expr();break;
+}
+return 0;
 }
 
 static char code_buf[65536];
