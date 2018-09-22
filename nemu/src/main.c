22646d86f7bd8312ec8d84ea436888c27bb6567a
tracer-ics2018 <tracer@njuics.org>
2018-09-22 09:29:15 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-1 (2018-07-05) x86_64 GNU/Linux  09:29:15 up 15:50,  1 user,  load average: 0.19, 0.08, 0.02 50b09395a2a6ae44da43aa9f38b37e824a01788

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<monitor/expr.h>
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
@@ -7,10 +8,14 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   char buff[65535];
+  char ah[50];
   FILE *fp=NULL;
   fp=fopen( " ~/ics2018/nemu/tools/gen-expr/input", "r");
   int t=5;
   while(t--){
+	  memset(ah,'\0',sizeof(ah));
+	  memset(buff,'\0',sizeof(buff));
+	  fscanf(fp,"%s",ah);
 	  fgets(buff,65535,(FILE*)fp);
 	  bool k=1;
 	  int z=expr(buff,&k);
