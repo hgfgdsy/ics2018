f9a23281958ff701dce42292fccfdeb3e8850bd6
tracer-ics2018 <tracer@njuics.org>
2018-10-17 20:04:47 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  20:04:47 up 1 day,  7:22,  8 users,  load average: 0.23, 0.09, 0.08 4fb6147adf20ed468355afb3af55da4e359cc289

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -42,7 +42,7 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm=instr_fetch(eip,op->width);
+  op->simm=(int)instr_fetch(eip,op->width);
 
   rtl_li(&op->val, op->simm);
 
