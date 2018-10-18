af3f34ed7eda4186474c3cb8410ab377ac90591e
tracer-ics2018 <tracer@njuics.org>
2018-10-18 22:24:42 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:24:42 up 1 day, 14:47,  8 users,  load average: 0.24, 0.19, 0.12 b0ccd6ffd09b249b7100ce0ffe267cf1a75533a2

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -46,6 +46,7 @@ extern make_EHelper(imul2);
 extern make_EHelper(add);
 extern make_EHelper(cltd);
 extern make_EHelper(idiv);
+extern make_EHelper(setcc);
 
 
 #define make_group(name, item0, item1, item2, item3, item4, item5, item6, item7) \
@@ -194,7 +195,7 @@ opcode_entry opcode_table [512] = {
   /* 0x88 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x8c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x94 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x94 */	IDEXW(r,setcc,1), EMPTY, EMPTY, EMPTY,
   /* 0x98 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x9c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa0 */	EMPTY, EMPTY, EMPTY, EMPTY,
