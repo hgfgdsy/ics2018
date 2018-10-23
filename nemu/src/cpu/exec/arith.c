ef986e2c1fae174065f726950c90de457c23a517
tracer-ics2018 <tracer@njuics.org>
2018-10-23 15:14:14 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  15:14:14 up 2 days,  2:14,  8 users,  load average: 0.37, 0.14, 0.08 f8bd3adaf5a67499d542535e6c0c273a17481b90

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -11,9 +11,7 @@ make_EHelper(add) {
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
-
   operand_write(id_dest,&t2);
-	
 	//TODO();
 
   print_asm_template2(add);
