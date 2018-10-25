7123b20fba57cc1af5566b03b33b83517e86aa69
tracer-ics2018 <tracer@njuics.org>
2018-10-26 00:10:22 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:10:22 up 2 days,  7:37,  8 users,  load average: 0.19, 0.11, 0.03 fb7c782f52ffa3f1b22b4b8e1a29c98335240fd

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,39 +3,108 @@
 #ifndef __ISA_NATIVE__
 
 size_t strlen(const char *s) {
-  return 0;
+  assert(s!=NULL);
+  int sum=0;
+  while(*s!='\0'){
+	  sum++;
+	  s++;}
+	return sum;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  assert((dst!=NULL)&&(src!=NULL));
+  char *adr=dst;
+  while((*dst++=*src++)!='\0')
+	NULL;
+  return adr;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *adr=dst;
+  assert((dst!=NULL)&&(src!=NULL));
+  if(strlen(src)<n){
+	  strcpy(dst,src);}
+  else{
+	  for(int i=0;i<n;i++){
+		  *dst=*src;
+		  dst++;
+		  src++;}
+	  *dst='\0';}
+	return adr;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *adr=dst;
+  assert((dst!=NULL)&&(src!=NULL));
+  while(*dst!='\0'){
+	  dst++;}
+  while(*src!='\0'){
+	  *dst=*src;
+	  dst++;
+	  src++;}
+  *dst='\0';
+  return adr;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int k=0;
+  assert((s1!=NULL)&&(s2!=NULL));
+  while((s1!='\0')&&(s2!='\0')){
+	  if(*s1>*s2){
+		  k=1;break;}
+	  else if(*s1<*s2){k=-1;break;}
+	  else{s1++;s2++;}}
+  if((*s1=='\0')&&(*s2=='\0'))
+	  k=0;
+  else if((*s1!='\0')&&(*s2=='\0'))
+	  k=1;
+  else if((*s1=='\0')&&(*s2!='\0'))
+	  k=-1;
+	return k;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  assert((s1!=NULL)&&(s2!=NULL));
+  int k=0;
+  if((strlen(s1)<=n)||(strlen(s2)<=n))
+	  k=strcmp(s1,s2);
+  else{
+	  for(int i=0;i<n;i++){
+          if(*s1>*s2){
+		  k=1;break;}
+	  else if(*s1<*s2){k=-1;break;}
+	  else{s1++;s2++;}}}
+	return k;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+	void* ret=v;
+	while(n--){
+		*(char*)v=(char)c;
+		v=(char*)v+1;}
+	  return ret;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  assert((out!=NULL)&&(in!=NULL));
+  char* bin=(char*)in;
+  char* bout=(char*)out;
+  while(n--){
+	  *bout++=*bin++;}
+	return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  int k=0;
+  assert((s1!=NULL)&&(s2!=NULL));
+  while((n--)&&(*(char*)s1==*(char*)s2)){
+	  s1=(char*)s1+1;;s2=(char*)s2+1;}
+  k=*(char*)s1-*(char*)s2;
+  if(k<0)
+	  return -1;
+  else if(k>0)
+	  return 1;
+	return 0;
 }
 
 #endif
