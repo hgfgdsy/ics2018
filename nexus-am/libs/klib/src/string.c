3665ff725031afa9298b67a20e705d7e9e3fb86a
tracer-ics2018 <tracer@njuics.org>
2018-10-26 00:36:38 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:36:38 up 2 days,  8:03,  8 users,  load average: 0.21, 0.12, 0.09 b32f2acfcd64409dec7450063dbc95b56355e1f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -49,18 +49,13 @@ char* strcat(char* dst, const char* src) {
 int strcmp(const char* s1, const char* s2) {
   int k=0;
   assert((s1!=NULL)&&(s2!=NULL));
-  while((s1!='\0')&&(s2!='\0')){
-	  if(*s1>*s2){
-		  k=1;break;}
-	  else if(*s1<*s2){k=-1;break;}
-	  else{s1++;s2++;}}
-  if((*s1=='\0')&&(*s2=='\0'))
-	  k=0;
-  else if((*s1!='\0')&&(*s2=='\0'))
-	  k=1;
-  else if((*s1=='\0')&&(*s2!='\0'))
-	  k=-1;
-	return k;
+  while(!(k=*(unsigned char*)s1-*(unsigned char*)s2)&&*s1){
+	  s1++;s2++;}
+  if(k<0)
+	  return -1;
+  else if(k>0)
+	  return 1;
+	return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
