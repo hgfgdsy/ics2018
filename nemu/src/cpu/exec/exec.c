abaa6354b2af44340f0ca79d39cccfe34d2e9139
tracer-ics2018 <tracer@njuics.org>
2018-10-25 11:57:08 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:57:08 up 2 days,  5:34,  8 users,  load average: 0.65, 0.51, 0.35 cc7b761107edd5b79b3d4e9baed3eca143e7975

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -30,6 +30,7 @@ static inline void idex(vaddr_t *eip, opcode_entry *e) {
 
 static make_EHelper(2byte_esc);
 
+extern make_EHelper(sbb);
 extern make_EHelper(call);
 extern make_EHelper(push);
 extern make_EHelper(sub);
@@ -111,7 +112,7 @@ opcode_entry opcode_table [512] = {
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
   /* 0x10 */	EMPTY, EMPTY, EMPTY, IDEX(E2G,adc),
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x18 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x18 */	EMPTY, EMPTY, EMPTY, IDEX(E2G,sbb),
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x20 */	EMPTY, EMPTY, IDEXW(E2G,and,1), EMPTY,
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
