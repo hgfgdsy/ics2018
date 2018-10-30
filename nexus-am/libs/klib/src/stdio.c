36cb22e8267063765444a189ad92326a4458b816
tracer-ics2018 <tracer@njuics.org>
2018-10-30 22:17:48 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:17:48 up  5:40, 11 users,  load average: 0.03, 0.10, 0.10 9cd60ea01e8def209066eb01723750426a104d88

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,7 +5,40 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+   int sum=0;
+   va_list ap;
+   va_start(ap,fmt);
+	while(*fmt!='\0'){
+	  if(*fmt=='%'){
+		  fmt++;
+		  switch(*fmt){
+			  case 'd':{fmt++;
+			  int k=va_arg(ap,int);
+			  char a[40];int label=0;int S;
+			  if(k>0){
+				  while(k>0){
+					  a[label++]=(k%10)+'0';
+					  k=k/10;}
+				  S=0;}
+			  else if(k==0){a[label++]='0';S=0;}
+			  else if(k<0){while(k<0){a[label++]=(-(k%10))+'0';k/=10;}S=1;}
+			  if(S==1){_putc('-');sum++;}
+			  for(int i=label-1;i>=0;i--){
+				  _putc(a[i]);sum++;}break;
+			  }
+			  case 's':{fmt++;
+			   char *src=va_arg(ap,char*);
+			   while(*src!='\0'){
+				   _putc(*src);sum++;}break;
+			   }
+	  }
+	  }
+	  else{
+		  _putc(*fmt);fmt++;sum++;}
+	  
+  }
+
+	return sum;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -19,7 +52,8 @@ int sprintf(char *out, const char *fmt, ...) {
   while(*fmt!='\0'){
 	  if(*fmt=='%'){
 		  fmt++;
-		  if(*fmt=='d'){fmt++;
+		  switch(*fmt){
+			  case 'd':{fmt++;
 			  int k=va_arg(ap,int);
 			  char a[40];int label=0;int S;
 			  if(k>0){
@@ -32,14 +66,15 @@ int sprintf(char *out, const char *fmt, ...) {
 			  if(S==1){*out='-';out++;sum++;}
 			  for(int i=label-1;i>=0;i--){
 				  *out=a[i];sum++;
-				  out++;}
+				  out++;}break;
 			  }
-                   else if(*fmt=='s'){fmt++;
+			  case 's':{fmt++;
 			   char *src=va_arg(ap,char*);
 			   while(*src!='\0'){
 				   *out=*src;sum++;
-				   out++;src++;}
+				   out++;src++;}break;
 			   }
+	  }
 	  }
 	  else{
 		  *out=*fmt;sum++;
