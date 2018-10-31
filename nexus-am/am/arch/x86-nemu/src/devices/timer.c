0a7a61f5b0b8484359daa1bf53587f95a5df1013
tracer-ics2018 <tracer@njuics.org>
2018-10-31 11:26:56 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:26:56 up  8:35, 14 users,  load average: 0.44, 0.21, 0.13 7541d350aa8cac321bf0ebb0bda9ab80d8eb8977

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -7,7 +7,7 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
       uptime->hi = 0;
-      uptime->lo = inl(0x48);
+      uptime->lo = 2001/*inl(0x48)*/;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
