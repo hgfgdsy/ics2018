6c62fdabb02744ce4fda78e5a5745fccd5dcf8df
tracer-ics2018 <tracer@njuics.org>
2018-10-31 11:25:22 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:25:22 up  8:33, 14 users,  load average: 0.18, 0.08, 0.08 6e2a04642521f57151f80e79ebbe10e732f59bc0

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -9,6 +9,7 @@ int main(){
   while (1) {
     while(uptime() < 1000 * sec) ;
     get_timeofday(&rtc);
+    printf("%d\n",uptime());
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
       printf("%d second).\n", sec);
