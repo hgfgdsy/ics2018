e808e1ec30d499861801f7c4426252d2c68f1d5c
tracer-ics2018 <tracer@njuics.org>
2018-11-04 09:38:52 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  09:38:52 up  1:36,  4 users,  load average: 0.29, 0.38, 0.33 9286733e464e6a0ca89939fad5eb61929cc853b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -109,7 +109,7 @@ static inline void interpret_rtl_setrelop(uint32_t relop, rtlreg_t *dest,
   *dest = interpret_relop(relop, *src1, *src2);
 }
 
-static inline void interpret_rtl_j(vaddr_t target) {
+/*static inline*/ void interpret_rtl_j(vaddr_t target) {
   cpu.eip = target;
   decoding_set_jmp(true);
 }
