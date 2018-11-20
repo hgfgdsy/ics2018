36fdb1fecc186eaa2c6096c2c2f9a3a64b3ad866
tracer-ics2018 <tracer@njuics.org>
2018-11-20 16:09:48 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:09:48 up  9:24,  1 user,  load average: 0.37, 0.17, 0.07 65bfa6234d5aa40e2b38d39e8eeb1fda1ca5ccee

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -29,7 +29,6 @@ typedef struct {
  struct
  {	rtlreg_t eax ,ecx, edx, ebx, esp, ebp, esi, edi;};
  };
-  uint32_t CS;
   vaddr_t eip;
   union{
 	  unsigned EFLAGS;
@@ -45,6 +44,7 @@ typedef struct {
 	  unsigned d4: 24;
   };
   };
+  uint32_t CS;
   uint16_t limit;
   uint32_t base;
 } CPU_state;
