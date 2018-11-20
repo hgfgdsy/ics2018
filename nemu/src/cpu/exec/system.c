052f10f6c0acede852e5500408b552c49bab14f5
tracer-ics2018 <tracer@njuics.org>
2018-11-20 17:08:25 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:08:25 up 10:23,  1 user,  load average: 0.00, 0.03, 0.13 3ab97ef667007826b0e8a60c726ebe3e6458655d

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -14,7 +14,7 @@ extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(lidt) {
   //TODO();
   cpu.limit=vaddr_read(cpu.eip,2);
-  cpu.base=vaddr_read(cpu.eip+1,4);
+  cpu.base=vaddr_read(cpu.eip+2,4);
   print_asm_template1(lidt);
 }
 
