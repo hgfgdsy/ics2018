87622ce56a76057b115d3c4aecc02b376efe36cc
tracer-ics2018 <tracer@njuics.org>
2018-11-20 16:47:14 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:47:14 up 10:02,  1 user,  load average: 0.12, 0.23, 0.46 dc19bc523044020be2b98fc76a13e7c9949b09

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,8 +10,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.EFLAGS);
   rtl_push(&cpu.CS);
   rtl_push(&ret_addr);
-  cpu.eip=vaddr_read(cpu.base+NO*8,2)+(vaddr_read(cpu.base+NO*8+4,2)<<16);
   assert(0);
+  cpu.eip=vaddr_read(cpu.base+NO*8,2)+(vaddr_read(cpu.base+NO*8+4,2)<<16);
+  //assert(0);
   decoding_set_jmp(true);
 }
 
