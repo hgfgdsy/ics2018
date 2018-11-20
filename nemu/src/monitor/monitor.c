0767aaea7a93bb8445207d4c803bf74a0ef81365
tracer-ics2018 <tracer@njuics.org>
2018-11-20 16:03:06 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:03:06 up  9:17,  1 user,  load average: 0.16, 0.04, 0.02 b4a9c5c07b08339dfcf9ffe3723c19a768225217

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,8 +85,7 @@ static inline long load_img() {
 
 static inline void restart() {
   /* Set the initial instruction pointer. */
-  cpu.EFLAGS=0;
-  cpu.d1=1;
+  cpu.EFLAGS=2;
   cpu.CS=8;
   cpu.eip = ENTRY_START;
 }
