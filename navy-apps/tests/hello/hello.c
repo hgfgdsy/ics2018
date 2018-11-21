9a1269b5bec7095f3d582d31788fa31a6cf7e479
tracer-ics2018 <tracer@njuics.org>
2018-11-21 19:34:56 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:34:56 up 18:07,  1 user,  load average: 0.08, 0.11, 0.05 a4b3cb9b59645dab2ec865026b6f7fe6131aa540

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,13 +4,14 @@
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
+  assert(0);
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 10000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
-      break;
+      //break;
     }
   }
   return 0;
