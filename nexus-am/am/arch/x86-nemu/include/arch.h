2d76e3c786d6fe845378eec232370ffa29088382
tracer-ics2018 <tracer@njuics.org>
2018-11-21 18:05:15 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:05:15 up 17:13,  1 user,  load average: 0.13, 0.03, 0.01 20c7c9dcac1c90dec94de0de6645aeb7f8f90536

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -21,11 +21,18 @@ struct _Context {
   //int       irq;
 };
 
+//#define GPR1 eax
+//#define GPR2 eip
+//#define GPR3 eip
+//#define GPR4 eip
+//#define GPRx eip
 #define GPR1 eax
-#define GPR2 eip
-#define GPR3 eip
-#define GPR4 eip
-#define GPRx eip
+#define GPR2 ebx
+#define GPR3 ecx
+#define GPR4 edx
+#define GPRx eax
+
+
 
 #ifdef __cplusplus
 extern "C" {
