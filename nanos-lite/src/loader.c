d50d2e1599f9eac33989965d73cb3b6373a7b357
tracer-ics2018 <tracer@njuics.org>
2018-11-21 13:39:56 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:39:56 up 15:20,  1 user,  load average: 0.09, 0.05, 0.01 14832171bd33a4b842ed1cce736c2c11b7315a2

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,8 +2,22 @@
 
 #define DEFAULT_ENTRY 0x4000000
 
+extern size_t ramdisk_read(void* buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+extern size_t ramdisk_write(const void* buf, size_t offset, size_t len);
+//extern void vaddr_write(vaddr_t addr, uint32_t data, int len);
+//extern uint8_t pmem[128*1024*1024];
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  //TODO();
+  void *buf=(void*)DEFAULT_ENTRY;
+  size_t len=get_ramdisk_size();
+  ramdisk_read(buf,0,len);
+  //uint32_t a;
+  //for(int i=0;i<len;i++){
+  //a=(uint8_t)buf[i];
+  //vaddr_write(DEFAULT_ENTRY+i,a,1);
+  //}
   return DEFAULT_ENTRY;
 }
 
