e2385d703b86fc8a0f34a94b3e5104132df12dee
tracer-ics2018 <tracer@njuics.org>
2018-11-21 19:30:10 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:30:10 up 18:03,  1 user,  load average: 0.11, 0.06, 0.01 d5a66eadf1b061efbbf421ffe620d071e7f9a2d2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,6 +10,11 @@ _Context* do_syscall(_Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
+	  case SYS_write: if(c->GPR2==1||c->GPR2==2){
+				  for(int i=0;i<c->GPR4;i++)
+					  _putc(*(char*)(c->GPR3+i));
+				  c->GPRx=c->GPR4;}
+			  break;
 	  case SYS_exit: _halt(c->GPRx); break;
 	  case SYS_yield: _yield(); c->GPRx=0;  break;
 	  default: panic("Unhandled syscall ID = %d", a[0]);
