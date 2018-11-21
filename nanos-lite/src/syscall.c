0b28a469dfa418ca577032511ebcf6f689259492
tracer-ics2018 <tracer@njuics.org>
2018-11-21 18:23:32 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:23:32 up 17:31,  1 user,  load average: 0.02, 0.03, 0.00 76858cf5569a78773f8fd3509d19cd80a2dd4988

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,16 @@
 #include "common.h"
 #include "syscall.h"
 
+//extern void _halt(int code) __attribute__((__noreturn__));
+
+
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+	  case SYS_exit: _halt(c->GPRx); break;
 	  case SYS_yield: _yield(); c->GPRx=0;  break;
 	  default: panic("Unhandled syscall ID = %d", a[0]);
   }
