91f73e3a1a4a86759aa668a7aad3232f4604e3a3
tracer-ics2018 <tracer@njuics.org>
2018-12-01 17:13:20 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  17:13:20 up  5:33, 19 users,  load average: 0.19, 0.10, 0.08 550c7253b9c3efd91e6638317f6dc29deca5ac4e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,6 +18,14 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  /*int key = read_key();
+  bool down = false;
+  if(key & 0x8000) {
+	  key ^= 0x8000;
+	  down = true;
+  }*/
+ // if(key != _KEY_NONE) {
+	 // return sprintf(buf, 
   return 0;
 }
 
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -37,7 +37,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
-  {"/dev/events", 0, 0, 0, event_read, NULL},
+  {"/dev/events", 0, 0, 0, NULL, NULL},
   {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
