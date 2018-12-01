198317d6ab66735a5aa58ade25eb707b67ba0601
tracer-ics2018 <tracer@njuics.org>
2018-12-01 14:59:53 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:59:53 up  3:19, 14 users,  load average: 0.15, 0.20, 0.18 62d4ff1858648291efe062823e7ed9ab61e621e1

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -127,6 +127,7 @@ static void get_display_info() {
   FILE *dispinfo = fopen("/proc/dispinfo", "r");
   assert(dispinfo);
   screen_w = screen_h = 0;
+  assert(0);
   char buf[128], key[128], value[128], *delim;
   while (fgets(buf, 128, dispinfo)) {
     *(delim = strchr(buf, ':')) = '\0';
@@ -135,7 +136,6 @@ static void get_display_info() {
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
   }
-  assert(0);
   fclose(dispinfo);
   assert(screen_w > 0 && screen_h > 0);
 }
