d583dad4eca49af024e462df19a1a32182b848db
tracer-ics2018 <tracer@njuics.org>
2018-12-01 11:30:15 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:30:15 up 16:57,  6 users,  load average: 0.28, 0.31, 0.31 f7c028bdafb38c4e84733779f388fa02aab0b2e

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -7,8 +7,8 @@ int main() {
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   assert(bmp->pixels);
   NDL_OpenDisplay(bmp->w, bmp->h);
-  assert(0);
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  assert(0);
   NDL_Render();
   NDL_CloseDisplay();
   while (1);
