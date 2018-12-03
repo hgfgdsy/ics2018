81b0fdf9d5b76bcc04ee395b978b0c11a67bfb9d
tracer-ics2018 <tracer@njuics.org>
2018-12-03 14:37:47 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  14:37:47 up  9:56, 19 users,  load average: 0.63, 0.61, 0.39 821a262219db56dbde58e9382d4805386f15ac4a

--- navy-apps/libs/libc/src/stdio/freopen.c
+++ navy-apps/libs/libc/src/stdio/freopen.c
@@ -217,7 +217,7 @@ _freopen_r (struct _reent *ptr,
 #endif
       return NULL;
     }
-
+  printf("%d %d\n", fp->_flags, flags);
   fp->_flags = flags;
   fp->_file = f;
   fp->_cookie = (void *) fp;
