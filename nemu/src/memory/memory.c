43acb7b487cd040dfb8a72095ef97de1c5f23c4c
tracer-ics2018 <tracer@njuics.org>
2018-12-20 16:38:26 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:38:26 up 1 day, 13 min,  7 users,  load average: 0.00, 0.02, 0.00 872e920ae42ee7c91f16441c61faaa883e24b7ec

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -19,9 +19,9 @@ paddr_t page_translate(vaddr_t addr){
 	printf("%x\t%x\n",cpu.cr3.val,addr);
 	uintptr_t my_PDA = (uintptr_t)((cpu.cr3.page_directory_base) + (((addr>>22)&0x3ff)<<2))<<12;
 	uint32_t my_pd = paddr_read(my_PDA,4);
-	printf("%x\t%x\n",my_pd,addr>>12);
+	printf("%x\t%x\n",my_pd,paddr_read(my_pd>>12,4));
 	assert((my_pd&0x1)==1);
-	uintptr_t my_PTA = (uintptr_t)(((my_pd>>12) & 0xfffff) +(((addr>>12)&0x3ff)<<2))<<12;
+	uintptr_t my_PTA = (uintptr_t)((my_pd>>12) +(((addr>>12)&0x3ff)<<2))<<12;
 	uint32_t my_pt = paddr_read(my_PTA,4);
 	printf("%x\n",my_pt);
 	assert((my_pt&0x1)==1);
