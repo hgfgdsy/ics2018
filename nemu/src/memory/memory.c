608253d97dc1043094c8797624efe1d597dbca3d
tracer-ics2018 <tracer@njuics.org>
2018-12-20 16:46:25 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:46:25 up 1 day, 21 min,  7 users,  load average: 0.13, 0.14, 0.05 a31af9476c9e6918c66ca153d87dbf54ca74c0c7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -52,7 +52,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
   if(cpu.cr0.paging){
-  if((addr&0xfff)>0x1000-(len<<2)){printf("???\n");assert(0);}
+  if((addr&0xfff)>0x1000-len){printf("???\n");assert(0);}
   else{
   paddr_t paddr=page_translate(addr);
   paddr_write(paddr, data, len);
