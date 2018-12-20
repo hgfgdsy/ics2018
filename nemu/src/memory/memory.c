3cd4312f58d48f18afe8e990416d299a183abc19
tracer-ics2018 <tracer@njuics.org>
2018-12-20 13:17:49 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:17:49 up 20:59,  7 users,  load average: 0.20, 0.23, 0.24 16accdb5982b9334b09c7f5fde6e1047eefa499

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -15,6 +15,8 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+//paddr_t page_tranlate(vaddr_t addr){
+
 uint32_t paddr_read(paddr_t addr, int len) {
   if(is_mmio(addr)==-1)
 	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
@@ -30,9 +32,17 @@ void paddr_write(paddr_t addr, uint32_t data, int len) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
+  //if((addr&0xfff)>0x1000-(len<<2)) assert(0);
+  //else{
+  //paddr_t paddr=page_translate(addr);
   return paddr_read(addr, len);
+  //}
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
+  //if((addr&0xfff)>0x1000-(len<<2)) assert(0);
+  //else{
+  //paddr_t paddr=page_translate(addr);
   paddr_write(addr, data, len);
+  //}
 }
