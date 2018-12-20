37b9b4fb5714e58cf57663f775f420c1e57e6bd0
tracer-ics2018 <tracer@njuics.org>
2018-12-20 13:29:57 +0800

    >  compile 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:29:57 up 21:11,  7 users,  load average: 0.44, 0.24, 0.20 63c9b74566e0ec758809ec2787ea6af12f2df9b7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -15,7 +15,15 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
-//paddr_t page_tranlate(vaddr_t addr){
+paddr_t page_translate(vaddr_t addr){
+	uintptr_t my_PDA = (uintptr_t)(cpu.cr3.page_directory_base) + (((addr>>22)&0x3ff)<<2);
+	uint32_t my_pd = paddr_read(my_PDA,4);
+	assert((my_pd&0x1)==1);
+	uintptr_t my_PTA = (uintptr_t)(((my_pd>>12) & 0x3fffff) + ((addr>>12)&0x3ff));
+	uint32_t my_pt = paddr_read(my_PTA,4);
+	assert((my_pt&0x1)==1);
+	return ((my_pt ^0x1) | (addr&0xfff));
+}
 
 uint32_t paddr_read(paddr_t addr, int len) {
   if(is_mmio(addr)==-1)
@@ -32,17 +40,17 @@ void paddr_write(paddr_t addr, uint32_t data, int len) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  //if((addr&0xfff)>0x1000-(len<<2)) assert(0);
-  //else{
-  //paddr_t paddr=page_translate(addr);
-  return paddr_read(addr, len);
-  //}
+  if((addr&0xfff)>0x1000-(len<<2)) assert(0);
+  else{
+  paddr_t paddr=page_translate(addr);
+  return paddr_read(paddr, len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  //if((addr&0xfff)>0x1000-(len<<2)) assert(0);
-  //else{
-  //paddr_t paddr=page_translate(addr);
-  paddr_write(addr, data, len);
-  //}
+  if((addr&0xfff)>0x1000-(len<<2)) assert(0);
+  else{
+  paddr_t paddr=page_translate(addr);
+  paddr_write(paddr, data, len);
+  }
 }
