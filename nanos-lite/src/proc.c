433cbb46d320374fe5057fcb8ba614a5c811b1bc
tracer-ics2018 <tracer@njuics.org>
2018-12-21 19:42:50 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  19:42:50 up 1 day,  9:57,  7 users,  load average: 0.39, 0.34, 0.27 5c738f0fc3c4673ba6099eae69c744d7432963c

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,16 +25,16 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   //naive_uload(NULL,"/bin/init");
-  //context_kload(&pcb[0],(void *)hello_fun);
-  context_uload(&pcb[0], "/bin/pal");
+  context_kload(&pcb[0],(void *)hello_fun);
+  context_uload(&pcb[1], "/bin/init");
   switch_boot_pcb();
 }
 
 _Context* schedule(_Context *prev) {
   //return NULL;
   current -> cp = prev;
-  current = &pcb[0];
-  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  //current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current -> cp;
 }
 
