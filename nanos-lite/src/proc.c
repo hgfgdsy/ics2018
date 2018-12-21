591f29de75aac4537b14d93f96933e05a9f0dc9a
tracer-ics2018 <tracer@njuics.org>
2018-12-21 21:51:07 +0800

    >  run 171240521 hgfgdsy Linux debian 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:51:07 up 1 day, 12:05,  7 users,  load average: 0.19, 0.22, 0.14 531f3045632be51412dcf4afda4077adffc0e6c7

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   //naive_uload(NULL,"/bin/init");
   //context_kload(&pcb[0],(void *)hello_fun);
-  context_uload(&pcb[0], "/bin/pal");
+  context_uload(&pcb[0], "/bin/dummy");
   switch_boot_pcb();
 }
 
